2d739e29315dfb195d70e6e8fdeaf9b182156795
hansongyan <hansongyan711@163.com>
2024-09-27 18:15:49 +0800

    debug

--- cmd.sh
+++ cmd.sh
@@ -1,3 +1,3 @@
-rm -rf /Users/yanhansong/git/igvwebpy/apps/bamView/static/bams/T23699.bqsr.bam*
-samtools view -h -b /Users/yanhansong/igv/bam/T23699.bqsr.bam chr7:55242462-55242475 -o /Users/yanhansong/git/igvwebpy/apps/bamView/static/bams/T23699.bqsr.bam
-samtools index /Users/yanhansong/git/igvwebpy/apps/bamView/static/bams/T23699.bqsr.bam
+rm -rf /public/workspace/yanhs2024/git/igvwebpy/apps/bamView/static/bams/OC03_A_20240912_RS1-NPNW-40912B-S1-02.bqsr.bam*
+samtools view -h -b /public/workspace/yanhs2024/project/Onc_project/14.project/B_analysis/analysis/OC03_A_20240912_RS1-NPNW-40912B-S1-02/aln/OC03_A_20240912_RS1-NPNW-40912B-S1-02.bqsr.bam chr13:32906559-32906569 -o /public/workspace/yanhs2024/git/igvwebpy/apps/bamView/static/bams/OC03_A_20240912_RS1-NPNW-40912B-S1-02.bqsr.bam
+samtools index /public/workspace/yanhs2024/git/igvwebpy/apps/bamView/static/bams/OC03_A_20240912_RS1-NPNW-40912B-S1-02.bqsr.bam
