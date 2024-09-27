e59b941baaaef12366847717f78c68738398b766
严寒松 <hansongyan711@163.com>
2024-09-27 17:27:28 +0800

    debug

--- cmd.sh
+++ cmd.sh
@@ -1,3 +1,3 @@
-rm -rf /Users/yanhansong/PycharmProjects/igv/apps/bamView/static/bams/T23699.bqsr.bam*
-samtools view -h -b /Users/yanhansong/igv/bam/T23699.bqsr.bam chr7:55242462-55242475 -o /Users/yanhansong/PycharmProjects/igv/apps/bamView/static/bams/T23699.bqsr.bam
-samtools index /Users/yanhansong/PycharmProjects/igv/apps/bamView/static/bams/T23699.bqsr.bam
+rm -rf /Users/yanhansong/git/igvwebpy/apps/bamView/static/bams/T23699.bqsr.bam*
+samtools view -h -b /Users/yanhansong/igv/bam/T23699.bqsr.bam chr7:55242462-55242475 -o /Users/yanhansong/git/igvwebpy/apps/bamView/static/bams/T23699.bqsr.bam
+samtools index /Users/yanhansong/git/igvwebpy/apps/bamView/static/bams/T23699.bqsr.bam
