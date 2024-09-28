326074bc50a1541ae9859eea04d4a0de6f2c7885
hansongyan <hansongyan711@163.com>
2024-09-28 08:37:56 +0800

    delete some cache

--- cmd.sh
+++
@@ -1,3 +0,0 @@
-rm -rf /public/workspace/yanhs2024/git/igvwebpy/apps/bamView/static/bams/OC03_A_20240912_RS1-NPNW-40912B-S1-02.bqsr.bam*
-samtools view -h -b /public/workspace/yanhs2024/project/Onc_project/14.project/B_analysis/analysis/OC03_A_20240912_RS1-NPNW-40912B-S1-02/aln/OC03_A_20240912_RS1-NPNW-40912B-S1-02.bqsr.bam chr13:32906559-32906569 -o /public/workspace/yanhs2024/git/igvwebpy/apps/bamView/static/bams/OC03_A_20240912_RS1-NPNW-40912B-S1-02.bqsr.bam
-samtools index /public/workspace/yanhs2024/git/igvwebpy/apps/bamView/static/bams/OC03_A_20240912_RS1-NPNW-40912B-S1-02.bqsr.bam
--- test.cmd
+++
@@ -1,2 +0,0 @@
-samtools view -h -b /Users/yanhansong/igv/bam/T23699.bqsr.bam chr7:55242462-55242475 -o /Users/yanhansong/PycharmProjects/igv/apps/bamView/static/bamView/bams/T23699.bqsr.bam
-fuck
